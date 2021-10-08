335414af0deaa24a03ab10192ebe4e96c5a9c9b7
Vlad Iovanov <vlad@aserto.com>
2021-10-08 08:07:10 +0300

    Fix typo

--- gh-action-entrypoint.sh
+++ gh-action-entrypoint.sh
@@ -5,7 +5,7 @@ export PRE_RELEASE="$INPUT_PRE_RELEASE"
 
 if [ -n "${INPUT_DOCKER_IMAGE}" ]; then
   version=$(/app/sver tags -s "${INPUT_DOCKER_REGISTRY}" -u "${INPUT_DOCKER_USERNAME}" -p "${INPUT_DOCKER_PASSWORD}" "${INPUT_DOCKER_IMAGE}")
-elif [ -n "${INPUT_NEXT}"]; then
+elif [ -n "${INPUT_NEXT}" ]; then
   version=$(/app/sver --next "${INPUT_NEXT}")
 else
   version=$(/app/sver)
